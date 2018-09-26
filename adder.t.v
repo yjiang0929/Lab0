60c5668a59407352b283613136532b560771a02c
NickShermeister <ncsherman159@gmail.com>
2018-09-26 19:00:03 -0400

    Slightly neater test bench now.

--- adder.t.v
+++ adder.t.v
@@ -22,8 +22,8 @@ initial begin
   $display("%b | %b | %b |   %b      |    %b     |  0001   | 0            | 0", a, b, sum, carryout, overflow);
   a = 4'b0000; b = 4'b0001; #`DELAY                                 // Set A and B, wait for update (#1)
   $display("%b | %b | %b |   %b      |    %b     |  0001   | 0            | 0", a, b, sum, carryout, overflow);
-  a = 4'b0100; b = 4'b0011; #`DELAY                                 // Set A and B, wait for update (#1)
-  $display("%b | %b | %b |   %b      |    %b     |  0111   | 0            | 0", a, b, sum, carryout, overflow);
+  a = 4'b0100; b = 4'b0100; #`DELAY                                 // Set A and B, wait for update (#1)
+  $display("%b | %b | %b |   %b      |    %b     |  1000   | 0            | 1", a, b, sum, carryout, overflow);
   a = 4'b0111; b = 4'b0111; #`DELAY                                 // Set A and B, wait for update (#1)
   $display("%b | %b | %b |   %b      |    %b     |  1110   | 0            | 1", a, b, sum, carryout, overflow);
 
@@ -32,8 +32,10 @@ initial begin
   $display(" A   |  B   | Sum  | Carryout | Overflow | Sum Exp | Carryout Exp | Overflow Exp");            // Prints header for truth table
   a = 4'b1111; b = 4'b1111; #`DELAY
   $display("%b | %b | %b |   %b      |    %b     |  1110   | 1            | 0", a, b, sum, carryout, overflow);
-  a = 4'b1101; b = 4'b1101; #`DELAY
-  $display("%b | %b | %b |   %b      |    %b     |  1010   | 1            | 0", a, b, sum, carryout, overflow);
+  a = 4'b1101; b = 4'b1111; #`DELAY
+  $display("%b | %b | %b |   %b      |    %b     |  1100   | 1            | 0", a, b, sum, carryout, overflow);
+  a = 4'b1111; b = 4'b1101; #`DELAY
+  $display("%b | %b | %b |   %b      |    %b     |  1100   | 1            | 0", a, b, sum, carryout, overflow);
   a = 4'b1001; b = 4'b1000; #`DELAY
   $display("%b | %b | %b |   %b      |    %b     |  0001   | 1            | 1", a, b, sum, carryout, overflow);
   a = 4'b1011; b = 4'b1011; #`DELAY
@@ -41,28 +43,17 @@ initial begin
 
   $display("\n\nFour Bit Full Adder negative + positive Tests");
   $display(" A   |  B   | Sum  | Carryout | Overflow | Sum Exp | Carryout Exp | Overflow Exp");            // Prints header for truth table
-  a = 4'b1001; b = 4'b0111; #`DELAY
-  $display("%b | %b | %b |   %b      |    %b     |  1000   | 1            | 1", a, b, sum, carryout, overflow);
   a = 4'b1000; b = 4'b0001; #`DELAY
   $display("%b | %b | %b |   %b      |    %b     |  1001   | 0            | 1", a, b, sum, carryout, overflow);
   a = 4'b1001; b = 4'b0000; #`DELAY
   $display("%b | %b | %b |   %b      |    %b     |  1001   | 0            | 1", a, b, sum, carryout, overflow);
-  a = 4'b1111; b = 4'b0111; #`DELAY
+  a = 4'b1111; b = 4'b0110; #`DELAY
   $display("%b | %b | %b |   %b      |    %b     |  0110   | 1            | 1", a, b, sum, carryout, overflow);
+  a = 4'b1001; b = 4'b0111; #`DELAY
+  $display("%b | %b | %b |   %b      |    %b     |  1000   | 1            | 1", a, b, sum, carryout, overflow);
 
 
-  $display("\n\nFour Bit Full Adder Overflow Tests");
-  $display(" A   |  B   | Sum  | Carryout | Overflow | Sum Exp | Carryout Exp | Overflow Exp");            // Prints header for truth table
-  a = 4'b1001; b = 4'b1111; #`DELAY
-  $display("%b | %b | %b |   %b      |    %b     |  1000   | 1            | 0", a, b, sum, carryout, overflow);
-  a = 4'b1111; b = 4'b0111; #`DELAY
-  $display("%b | %b | %b |   %b      |    %b     |  0110   | 1            | 1", a, b, sum, carryout, overflow);
-  a = 4'b1001; b = 4'b0000; #`DELAY
-  $display("%b | %b | %b |   %b      |    %b     |  1001   | 0            | 1", a, b, sum, carryout, overflow);
-  a = 4'b0110; b = 4'b0001; #`DELAY
-  $display("%b | %b | %b |   %b      |    %b     |  0111   | 0            | 0", a, b, sum, carryout, overflow);
-
-  $display("\n\nFour Bit Full Adder Carryout Tests");
+  $display("\n\nFour Bit Full Adder Overflow/Carryout Tests");
   $display(" A   |  B   | Sum  | Carryout | Overflow | Sum Exp | Carryout Exp | Overflow Exp");            // Prints header for truth table
   a = 4'b1001; b = 4'b1111; #`DELAY
   $display("%b | %b | %b |   %b      |    %b     |  1000   | 1            | 0", a, b, sum, carryout, overflow);
@@ -70,9 +61,8 @@ initial begin
   $display("%b | %b | %b |   %b      |    %b     |  0110   | 1            | 1", a, b, sum, carryout, overflow);
   a = 4'b0111; b = 4'b0001; #`DELAY
   $display("%b | %b | %b |   %b      |    %b     |  1000   | 0            | 1", a, b, sum, carryout, overflow);
-  a = 4'b0101; b = 4'b0000; #`DELAY
-  $display("%b | %b | %b |   %b      |    %b     |  0101   | 0            | 0", a, b, sum, carryout, overflow);
-
+  a = 4'b0110; b = 4'b0001; #`DELAY
+  $display("%b | %b | %b |   %b      |    %b     |  0111   | 0            | 0", a, b, sum, carryout, overflow);
 
 end
 //For hte 16 test cases, we want:
