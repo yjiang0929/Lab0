44339f1918d02c52309e47b4a68b88b892b89bf5
NickShermeister <ncsherman159@gmail.com>
2018-09-26 19:19:57 -0400

    corrected.

--- adder.t.v
+++ adder.t.v
@@ -44,20 +44,20 @@ initial begin
   $display("\n\nFour Bit Full Adder negative + positive Tests");
   $display(" A   |  B   | Sum  | Carryout | Overflow | Sum Exp | Carryout Exp | Overflow Exp");            // Prints header for truth table
   a = 4'b1000; b = 4'b0001; #`DELAY
-  $display("%b | %b | %b |   %b      |    %b     |  1001   | 0            | 1", a, b, sum, carryout, overflow);
+  $display("%b | %b | %b |   %b      |    %b     |  1001   | 0            | 0", a, b, sum, carryout, overflow);
   a = 4'b1001; b = 4'b0000; #`DELAY
-  $display("%b | %b | %b |   %b      |    %b     |  1001   | 0            | 1", a, b, sum, carryout, overflow);
-  a = 4'b1111; b = 4'b0110; #`DELAY
-  $display("%b | %b | %b |   %b      |    %b     |  0110   | 1            | 1", a, b, sum, carryout, overflow);
+  $display("%b | %b | %b |   %b      |    %b     |  1001   | 0            | 0", a, b, sum, carryout, overflow);
   a = 4'b1001; b = 4'b0111; #`DELAY
-  $display("%b | %b | %b |   %b      |    %b     |  1000   | 1            | 1", a, b, sum, carryout, overflow);
+  $display("%b | %b | %b |   %b      |    %b     |  0000   | 1            | 0", a, b, sum, carryout, overflow);
+  a = 4'b1110; b = 4'b0111; #`DELAY
+  $display("%b | %b | %b |   %b      |    %b     |  0101   | 1            | 0", a, b, sum, carryout, overflow);
 
 
   $display("\n\nFour Bit Full Adder Overflow/Carryout Tests");
   $display(" A   |  B   | Sum  | Carryout | Overflow | Sum Exp | Carryout Exp | Overflow Exp");            // Prints header for truth table
   a = 4'b1001; b = 4'b1111; #`DELAY
   $display("%b | %b | %b |   %b      |    %b     |  1000   | 1            | 0", a, b, sum, carryout, overflow);
-  a = 4'b1111; b = 4'b0111; #`DELAY
+  a = 4'b1111; b = 4'b1000; #`DELAY
   $display("%b | %b | %b |   %b      |    %b     |  0110   | 1            | 1", a, b, sum, carryout, overflow);
   a = 4'b0111; b = 4'b0001; #`DELAY
   $display("%b | %b | %b |   %b      |    %b     |  1000   | 0            | 1", a, b, sum, carryout, overflow);
